5453f1f4b910979b1d116e6a4273c05a54c571a4
Alfheim <281508958@qq.com>
2019-07-01 19:22:48 +0800

    Create No.74 Search a 2D Matrix.cpp

---
+++ Cpp/Submit Records/No.74 Search a 2D Matrix.cpp
@@ -0,0 +1,36 @@
+/********************************************
+作者:Alfeim
+题目:搜索二维矩阵
+时间消耗:12ms
+解题思路:递归、二分查找
+********************************************/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {  
+        return Solve(matrix,0,target);
+    }
+    
+    bool Solve(vector<vector<int>>& matrix,int row,int target){
+        if(row >= matrix.size() || matrix[row].empty())
+            return false;
+        
+        if(target > matrix[row][matrix[0].size()-1])
+            return Solve(matrix,row + 1,target);
+        
+        return Search(matrix[row],0,matrix[row].size()-1,target);
+    }
+    
+    bool Search(vector<int> Row,int begin,int end,int target){
+        int mid = (begin + end)/2;
+        
+        if(begin > end)
+            return false;
+        
+        if(Row[mid] == target)
+            return true;
+        
+        return target > Row[mid] ? Search(Row,mid+1,end,target) : Search(Row,begin,mid-1,target);
+        
+    }
+
+};
