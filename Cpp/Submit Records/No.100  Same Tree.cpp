0830e1f1d05ac08ece6de19467b0e6fa2baaeab9
Alfheim <281508958@qq.com>
2019-07-03 14:27:21 +0800

    Create No.100  Same Tree.cpp

---
+++ Cpp/Submit Records/No.100  Same Tree.cpp
@@ -0,0 +1,22 @@
+/********************************************
+作者:Alfeim
+题目:搜索旋转排序数组
+时间消耗:4ms
+解题思路:递归
+********************************************/
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q )
+            return true;
+        
+        if( (p && !q) || (!p && q))
+            return false;
+        
+        if(p->val != q->val)
+            return false;
+        
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+
+    }
+};
